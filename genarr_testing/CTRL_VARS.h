68c29c832b24f6d8a10b4d0cbfe4c93c242d458d
Shreyas911 <shreyasg911@gmail.com>
2023-07-14 15:23:58 -0500

    A nice working example of how ctrl parameters can be read from a header file.

---
+++ genarr_testing/CTRL_VARS.h
@@ -0,0 +1,8 @@
+#define STRLENGTH	    15
+#define NUM_CTRL	    2
+#define xx_genarr2d_vars    (/ 'xx_cslide      ', \
+			       'xx_qgeo        ' /)
+#define xx_genarr2d_weights (/ '1.0            ', \
+			       '               ' /)
+#define xx_genarr2d_bounds  (/ '  2, 10,  5, 60', \
+			       '               ' /)
--- genarr_testing/driver.F90
+++ genarr_testing/driver.F90
@@ -1,9 +1,49 @@
-program driver
-
 #include "CTRL_SIZE.h"
+#include "CTRL_VARS.h"
+
+#define IMAX 84
+#define JMAX 144
+
+program driver
 
 implicit none
 
-print *, maxCtrlArr2D
+character(STRLENGTH), dimension(NUM_CTRL) :: vars_aux
+character(STRLENGTH), dimension(NUM_CTRL) :: weights_aux 
+character(STRLENGTH), dimension(NUM_CTRL) :: bounds_aux
+real(8),              dimension(NUM_CTRL) :: weights
+integer                                   :: i
+integer(4),           dimension(NUM_CTRL) :: xLow, xHigh, yLow, yHigh
+
+vars_aux    = xx_genarr2d_vars
+weights_aux = xx_genarr2d_weights
+bounds_aux  = xx_genarr2d_bounds
+
+do i=1, NUM_CTRL
+        if (weights_aux(i) .NE. ' ') then
+
+                read (unit=weights_aux(i),fmt=*) weights(i)
+  
+        end if
+
+        if (bounds_aux(i) .NE. ' ') then
+
+                read (unit=bounds_aux(i),fmt=*) xLow(i), xHigh(i), yLow(i), yHigh(i) 
+
+        else
+
+                xLow(i)  = 1
+                xHigh(i) = IMAX+1
+                yLow(i)  = 1
+                yHigh(i) = JMAX+1 
+
+        end if
+end do
+
+print *, weights
+print *, xLow
+print *, xHigh
+print *, yLow
+print *, yHigh
 
 end program driver
