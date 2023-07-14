6af87c4674e2b44253d87873b08652ccbe15a0cb
Shreyas911 <shreyasg911@gmail.com>
2023-07-14 13:12:44 -0500

    Initial commit for testing some genctrl and gencost stuff.

---
+++ genarr_testing/CTRL_SIZE.h
@@ -0,0 +1,4 @@
+#define maxCtrlArr2D 2
+#define maxCtrlArr3D 2
+#define maxCtrlTim2D 9
+#define maxCtrlProc 1
---
+++ genarr_testing/driver.F90
@@ -0,0 +1,9 @@
+program driver
+
+#include "CTRL_SIZE.h"
+
+implicit none
+
+print *, maxCtrlArr2D
+
+end program driver
